800a392e444c731379e256f94eed19e23c540abd
eltacitos <hugo.locatelli@epita.fr>
2022-09-21 19:31:01 +0200

    [INIT][INIT] of the project

--- src/main.py
+++ src/main.py
@@ -1,25 +1,32 @@
 from discord.ext import commands
 import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 bot = commands.Bot(
     command_prefix="!",  # Change to desired prefix
-    case_insensitive=True, # Commands aren't case-sensitive
-    intents = intents # Set up basic permissions
+    case_insensitive=True,  # Commands aren't case-sensitive
+    intents=intents  # Set up basic permissions
 )
 
-bot.author_id = 0000000  # Change to your discord id
+bot.author_id = 315509578924032010  # Change to your discord id
+
 
 @bot.event
 async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
+
 @bot.command()
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_TOKEN>"
+
+token = os.getenv("TOKEN")
 bot.run(token)  # Starts the bot
