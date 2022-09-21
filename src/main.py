110fe3cef524489306fc4c70b7e837b5408bc605
eltacitos <hugo.locatelli@epita.fr>
2022-09-21 20:15:40 +0200

    [REFACTOR][WARMUP] Moved warmup code to a new file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+import asyncio
+
 from discord.ext import commands
 import discord
-import random
 import os
 from dotenv import load_dotenv
+
+from src.warmup import Warmup
 
 load_dotenv()
 
@@ -21,6 +24,7 @@ bot.author_id = 315509578924032010  # Change to your discord id
 
 @bot.event
 async def on_ready():  # When the bot is ready
+    await bot.add_cog(Warmup(bot))
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
@@ -30,21 +34,4 @@ async def pong(ctx):
     await ctx.send('pong')
 
 
-@bot.command()
-async def name(ctx):
-    await ctx.send(ctx.author.name)
-
-
-@bot.command()
-async def d6(ctx):
-    await ctx.send(random.randint(1, 6))
-
-
-@bot.event
-async def on_message(message):
-    if message.content == "Salut tout le monde":
-        await message.channel.send("Salut tout seul")
-
-
-token = os.getenv("TOKEN")
-bot.run(token)  # Starts the bot
+bot.run(os.getenv("TOKEN"))
---
+++ src/warmup.py
@@ -0,0 +1,28 @@
+import random
+
+from discord.ext import commands
+
+
+class Warmup(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot  # now you'll use self.bot instead of just bot when referring to the bot in the code
+
+    # this is how you register events, instead of using @bot.event
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content == "Salut tout le monde":
+            await message.channel.send("Salut tout seul")
+
+    @commands.command()
+    async def name(self, message):
+        await message.channel.send(message.author.name)
+
+    @commands.command()
+    async def d6(self, message):
+        await message.channel.send(random.randint(1, 6))
+
+
+# this setup function needs to be in every cog in order for the bot to be able to load it
+def setup(bot):
+    bot.add_cog(Warmup(bot))
